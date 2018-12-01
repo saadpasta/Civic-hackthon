ad6086d4e211fa36eff89c5513b2c712c8f29350
saadpasta <saadpasta70@gmaail.com>
2018-12-01 13:17:16 +0500

    login boiler plate

--- Navigation/AppNavigator.js
+++ Navigation/AppNavigator.js
@@ -1,12 +1,14 @@
 import MapScreen from '../Screens/MapScreens/MapScreen'
+import LoginScreenUser from '../Screens/LoginScreens/LoginUser'
 import { createDrawerNavigator, createMaterialTopTabNavigator, createStackNavigator, createAppContainer } from "react-navigation";
 
 const StackNavigator = createStackNavigator({
+    Login: { screen: LoginScreenUser },
     Map: {
         screen: MapScreen,
     },
 })
-  
+
 const Navigator = createAppContainer(StackNavigator)
 
-export default Navigator
+export default Navigator;
